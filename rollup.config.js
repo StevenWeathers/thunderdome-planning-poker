9aa379994c9133246bca967a5dbc87e923b5b39a
Steven Weathers <steven@weathers.me>
2019-07-18 19:56:57 -0400

    Update js bundle date

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
     sourcemap: false,
     format: 'iife',
     name: 'app',
-    file: 'dist/js/bundle.071019.js'
+    file: 'dist/js/bundle.071819.js'
   },
   plugins: [
     del({ targets: 'dist/*' }),
