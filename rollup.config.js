ea8147228504aff8953289e110a1822f20b00ae7
Steven Weathers <steven@weathers.me>
2019-07-18 23:48:47 -0400

    Bump js bundle date

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
     sourcemap: false,
     format: 'iife',
     name: 'app',
-    file: 'dist/js/bundle.071819.js'
+    file: 'dist/js/bundle.071919.js'
   },
   plugins: [
     del({ targets: 'dist/*' }),
