eb76c59a7ce9cdeae4c0955c1f4f9fab4afe699e
Steven Weathers <steven@weathers.me>
2020-04-02 23:06:18 -0400

    Remove storybook

--- .storybook/addons.js
+++
@@ -1,6 +0,0 @@
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-actions/register'
-import '@storybook/addon-storysource/register'
-import '@storybook/addon-notes/register'
-import '@storybook/addon-viewport/register'
-import '@storybook/addon-a11y/register'
--- .storybook/postcss.config.js
+++
@@ -1,9 +0,0 @@
-var tailwindcss = require('tailwindcss')
-
-module.exports = {
-    plugins: [
-        require('postcss-import')(),
-        tailwindcss('./tailwind.config.js'),
-        require('autoprefixer'),
-    ],
-}
