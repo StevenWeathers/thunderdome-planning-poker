7b401ae48209bb53b5f0b64c815f1284ca33570c
Steven Weathers <steven@weathers.me>
2020-10-11 10:41:28 -0400

    Add WYSIWYG using Quill for Description and AC
    This resolves #85 by adding Quill WYSIWYG to Plan Description and AC fields.

--- frontend/src/components/AddPlan.svelte
+++ frontend/src/components/AddPlan.svelte
@@ -1,6 +1,8 @@
 <script>
     import TurndownService from 'turndown'
+    import marked from 'marked'
 
+    import { quill } from '../quill'
     import SolidButton from './SolidButton.svelte'
     import CloseIcon from './icons/CloseIcon.svelte'
     import DownCarrotIcon from './icons/DownCarrotIcon.svelte'
@@ -42,133 +44,132 @@
     }
 </script>
 
-<div class="fixed inset-0 flex items-center z-40">
+<div
+    class="fixed inset-0 flex items-center z-40 max-h-screen overflow-y-scroll">
     <div class="fixed inset-0 bg-gray-900 opacity-75"></div>
 
     <div
-        class="relative mx-4 md:mx-auto w-full md:w-2/3 lg:w-3/5 xl:w-1/3 z-50
-        m-8">
-        <div class="shadow-xl bg-white rounded-lg p-4 xl:p-6">
-            <div class="flex justify-end mb-2">
-                <button
-                    aria-label="close"
-                    on:click="{toggleAddPlan}"
-                    class="text-gray-800">
-                    <CloseIcon />
-                </button>
-            </div>
+        class="relative mx-4 md:mx-auto w-full md:w-2/3 lg:w-3/5 xl:w-1/2 z-50
+        max-h-full">
+        <div class="py-8">
+            <div class="shadow-xl bg-white rounded-lg p-4 xl:p-6 max-h-full">
+                <div class="flex justify-end mb-2">
+                    <button
+                        aria-label="close"
+                        on:click="{toggleAddPlan}"
+                        class="text-gray-800">
+                        <CloseIcon />
+                    </button>
+                </div>
 
-            <form on:submit="{handleSubmit}" name="addPlan">
-                <div class="mb-4">
-                    <label class="block text-sm font-bold mb-2" for="planName">
-                        Plan Type
-                    </label>
-                    <div class="relative">
-                        <select
-                            name="planType"
-                            bind:value="{planType}"
-                            required
-                            class="block appearance-none w-full border-2
-                            border-gray-400 text-gray-700 py-3 px-4 pr-8 rounded
-                            leading-tight focus:outline-none
-                            focus:border-purple-500">
-                            <option value="" disabled>Types</option>
-                            {#each planTypes as pType}
-                                <option value="{pType}">{pType}</option>
-                            {/each}
-                        </select>
-                        <div
-                            class="pointer-events-none absolute inset-y-0
-                            right-0 flex items-center px-2 text-gray-700">
-                            <DownCarrotIcon />
+                <form on:submit="{handleSubmit}" name="addPlan">
+                    <div class="mb-4">
+                        <label
+                            class="block text-sm font-bold mb-2"
+                            for="planName">
+                            Plan Type
+                        </label>
+                        <div class="relative">
+                            <select
+                                name="planType"
+                                bind:value="{planType}"
+                                required
+                                class="block appearance-none w-full border-2
+                                border-gray-400 text-gray-700 py-3 px-4 pr-8
+                                rounded leading-tight focus:outline-none
+                                focus:border-purple-500">
+                                <option value="" disabled>Types</option>
+                                {#each planTypes as pType}
+                                    <option value="{pType}">{pType}</option>
+                                {/each}
+                            </select>
+                            <div
+                                class="pointer-events-none absolute inset-y-0
+                                right-0 flex items-center px-2 text-gray-700">
+                                <DownCarrotIcon />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="mb-4">
-                    <label class="block text-sm font-bold mb-2" for="planName">
-                        Plan Name
-                    </label>
-                    <input
-                        class="bg-gray-200 border-gray-200 border-2
-                        appearance-none rounded w-full py-2 px-3 text-gray-700
-                        leading-tight focus:outline-none focus:bg-white
-                        focus:border-purple-500"
-                        type="text"
-                        id="planName"
-                        name="planName"
-                        bind:value="{planName}"
-                        placeholder="Enter a plan name" />
-                </div>
-                <div class="mb-4">
-                    <label
-                        class="block text-sm font-bold mb-2"
-                        for="referenceId">
-                        Reference ID
-                    </label>
-                    <input
-                        class="bg-gray-200 border-gray-200 border-2
-                        appearance-none rounded w-full py-2 px-3 text-gray-700
-                        leading-tight focus:outline-none focus:bg-white
-                        focus:border-purple-500"
-                        type="text"
-                        id="referenceId"
-                        name="referenceId"
-                        bind:value="{referenceId}"
-                        placeholder="Enter a reference ID" />
-                </div>
-                <div class="mb-4">
-                    <label class="block text-sm font-bold mb-2" for="planLink">
-                        Link
-                    </label>
-                    <input
-                        class="bg-gray-200 border-gray-200 border-2
-                        appearance-none rounded w-full py-2 px-3 text-gray-700
-                        leading-tight focus:outline-none focus:bg-white
-                        focus:border-purple-500"
-                        type="text"
-                        id="planLink"
-                        name="planLink"
-                        bind:value="{planLink}"
-                        placeholder="Enter a link to story" />
-                </div>
-                <div class="mb-4">
-                    <label
-                        class="block text-sm font-bold mb-2"
-                        for="description">
-                        Description
-                    </label>
-                    <textarea
-                        class="bg-gray-200 border-gray-200 border-2
-                        appearance-none rounded w-full py-2 px-3 text-gray-700
-                        leading-tight focus:outline-none focus:bg-white
-                        focus:border-purple-500"
-                        id="description"
-                        name="description"
-                        bind:value="{description}"
-                        placeholder="Enter a plan description"></textarea>
-                </div>
-                <div class="mb-4">
-                    <label
-                        class="block text-sm font-bold mb-2"
-                        for="acceptanceCriteria">
-                        Acceptance Criteria
-                    </label>
-                    <textarea
-                        class="bg-gray-200 border-gray-200 border-2
-                        appearance-none rounded w-full py-2 px-3 text-gray-700
-                        leading-tight focus:outline-none focus:bg-white
-                        focus:border-purple-500"
-                        id="acceptanceCriteria"
-                        name="acceptanceCriteria"
-                        bind:value="{acceptanceCriteria}"
-                        placeholder="Enter a plan acceptance criteria"></textarea>
-                </div>
-                <div class="text-right">
-                    <div>
-                        <SolidButton type="submit">Save</SolidButton>
+                    <div class="mb-4">
+                        <label
+                            class="block text-sm font-bold mb-2"
+                            for="planName">
+                            Plan Name
+                        </label>
+                        <input
+                            class="bg-gray-200 border-gray-200 border-2
+                            appearance-none rounded w-full py-2 px-3
+                            text-gray-700 leading-tight focus:outline-none
+                            focus:bg-white focus:border-purple-500"
+                            type="text"
+                            id="planName"
+                            name="planName"
+                            bind:value="{planName}"
+                            placeholder="Enter a plan name" />
                     </div>
-                </div>
-            </form>
+                    <div class="mb-4">
+                        <label
+                            class="block text-sm font-bold mb-2"
+                            for="referenceId">
+                            Reference ID
+                        </label>
+                        <input
+                            class="bg-gray-200 border-gray-200 border-2
+                            appearance-none rounded w-full py-2 px-3
+                            text-gray-700 leading-tight focus:outline-none
+                            focus:bg-white focus:border-purple-500"
+                            type="text"
+                            id="referenceId"
+                            name="referenceId"
+                            bind:value="{referenceId}"
+                            placeholder="Enter a reference ID" />
+                    </div>
+                    <div class="mb-4">
+                        <label
+                            class="block text-sm font-bold mb-2"
+                            for="planLink">
+                            Link
+                        </label>
+                        <input
+                            class="bg-gray-200 border-gray-200 border-2
+                            appearance-none rounded w-full py-2 px-3
+                            text-gray-700 leading-tight focus:outline-none
+                            focus:bg-white focus:border-purple-500"
+                            type="text"
+                            id="planLink"
+                            name="planLink"
+                            bind:value="{planLink}"
+                            placeholder="Enter a link to story" />
+                    </div>
+                    <div class="mb-16">
+                        <div class="text-sm font-bold mb-2">Description</div>
+                        <div class="h-48">
+                            <div
+                                class="w-full"
+                                use:quill="{{ placeholder: 'Enter a plan description', content: marked(description) }}"
+                                on:text-change="{e => (description = e.detail.html)}"
+                                id="description"></div>
+                        </div>
+                    </div>
+                    <div class="mb-16">
+                        <div class="text-sm font-bold mb-2">
+                            Acceptance Criteria
+                        </div>
+                        <div class="h-48">
+                            <div
+                                class="w-full"
+                                use:quill="{{ placeholder: 'Enter plan acceptance criteria', content: marked(acceptanceCriteria) }}"
+                                on:text-change="{e => (acceptanceCriteria = e.detail.html)}"
+                                id="acceptanceCriteria"></div>
+                        </div>
+                    </div>
+                    <div class="text-right">
+                        <div>
+                            <SolidButton type="submit">Save</SolidButton>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
---
+++ frontend/src/quill.js
@@ -0,0 +1,37 @@
+import Quill from 'quill'
+
+function quill(node, options) {
+    const quill = new Quill(node, {
+        modules: {
+            toolbar: [
+                ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+                ['blockquote', 'code-block'],
+                [{ list: 'ordered' }, { list: 'bullet' }],
+                [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+                [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+                [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+                [{ align: [] }],
+                ['clean'], // remove formatting button
+            ],
+        },
+        placeholder: 'Type something...',
+        theme: 'snow',
+        ...options,
+    })
+    const container = node.getElementsByClassName('ql-editor')[0]
+    container.innerHTML = options.content
+
+    quill.on('text-change', function(delta, oldDelta, source) {
+        node.dispatchEvent(
+            new CustomEvent('text-change', {
+                detail: {
+                    html: container.innerHTML,
+                    text: quill.getText(),
+                },
+            }),
+        )
+    })
+}
+
+export { quill }
