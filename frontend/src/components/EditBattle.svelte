3076f49bcb612f71eec0f0fdfbf876e2bec597eb
tnonnenm <tt@teetee.de>
2021-05-29 00:53:58 +0200

    Fix German language pack and translate rounding mode drop down items

--- frontend/src/components/EditBattle.svelte
+++ frontend/src/components/EditBattle.svelte
@@ -102,7 +102,7 @@
                     id="averageRounding"
                     name="averageRounding">
                     {#each allowedPointAverages as item}
-                        <option value="{item}">{item}</option>
+                        <option value="{item}">{$_('pages.myBattles.createBattle.fields.averageRounding.' + item)}</option>
                     {/each}
                 </select>
                 <div
