2b9ae2e1e70e0fde47d459aa0c16b768c253e51d
Steven Weathers <steven@weathers.me>
2021-10-28 19:27:07 -0400

    Change country flag image source
    Due to original API having gone down for who knows how long

--- frontend/src/components/CountryFlag.svelte
+++ frontend/src/components/CountryFlag.svelte
@@ -3,13 +3,17 @@
     import { _ } from '../i18n'
 
     export let country = ''
-    export let size = 64
     export let additionalClass = ''
 </script>
 
 <img
-    src="https://www.countryflags.io/{country.toLowerCase()}/shiny/{size}.png"
+    src="https://flagcdn.com/64x48/{country.toLowerCase()}.png"
+    srcset="https://flagcdn.com/128x96/{country.toLowerCase()}.png 2x,
+    https://flagcdn.com/192x144/{country.toLowerCase()}.png 3x"
+    width="64"
+    height="48"
     alt="{countryMap[country]}
     {$_('flag')}"
     title="{countryMap[country]}"
-    class="{additionalClass}" />
+    class="{additionalClass}"
+/>
