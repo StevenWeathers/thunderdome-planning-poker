ec0c2f58fde90896b95cfc25490a09e330a36163
Steven Weathers <steven@weathers.me>
2021-11-20 09:54:20 -0500

    Update indexes migrations to use constraints for all but user email

--- db/migrations/20211120142008_create_unique_indexes.up.sql
+++ db/migrations/20211120142008_create_unique_indexes.up.sql
@@ -1,10 +1,14 @@
 CREATE UNIQUE INDEX IF NOT EXISTS email_unique_idx
     ON users (LOWER(email));
-CREATE UNIQUE INDEX IF NOT EXISTS api_keys_warrior_id_name_key
-	ON api_keys (user_id, name);
-CREATE UNIQUE INDEX IF NOT EXISTS organization_team_team_id_key
-	ON organization_team (team_id);
-CREATE UNIQUE INDEX IF NOT EXISTS organization_department_organization_id_name_key
-    ON organization_department (organization_id, name);
-CREATE UNIQUE INDEX IF NOT EXISTS department_team_team_id_key
-	ON department_team (team_id);
+ALTER TABLE api_keys DROP CONSTRAINT IF EXISTS api_keys_warrior_id_name_key;
+ALTER TABLE api_keys ADD CONSTRAINT api_keys_warrior_id_name_key
+	UNIQUE (user_id, name);
+ALTER TABLE organization_team DROP CONSTRAINT IF EXISTS organization_team_team_id_key;
+ALTER TABLE organization_team ADD CONSTRAINT organization_team_team_id_key
+    UNIQUE (team_id);
+ALTER TABLE organization_department DROP CONSTRAINT IF EXISTS organization_department_organization_id_name_key;
+ALTER TABLE organization_department ADD CONSTRAINT organization_department_organization_id_name_key
+    UNIQUE (organization_id, name);
+ALTER TABLE department_team DROP CONSTRAINT IF EXISTS department_team_team_id_key;
+ALTER TABLE department_team ADD CONSTRAINT department_team_team_id_key
+    UNIQUE (team_id);
