e76137a380b82581b39cea0e9f540439e72c5c66
Steven Weathers <steven@weathers.me>
2021-11-20 09:55:15 -0500

    Update apikeys table constraint name

--- db/migrations/20211120142008_create_unique_indexes.up.sql
+++ db/migrations/20211120142008_create_unique_indexes.up.sql
@@ -1,7 +1,7 @@
 CREATE UNIQUE INDEX IF NOT EXISTS email_unique_idx
     ON users (LOWER(email));
 ALTER TABLE api_keys DROP CONSTRAINT IF EXISTS api_keys_warrior_id_name_key;
-ALTER TABLE api_keys ADD CONSTRAINT api_keys_warrior_id_name_key
+ALTER TABLE api_keys ADD CONSTRAINT api_keys_user_id_name_key
 	UNIQUE (user_id, name);
 ALTER TABLE organization_team DROP CONSTRAINT IF EXISTS organization_team_team_id_key;
 ALTER TABLE organization_team ADD CONSTRAINT organization_team_team_id_key
