6a07ae2adb958cad3668623281ad188045ead550
Steven Weathers <steven@weathers.me>
2021-11-20 20:22:52 -0500

    Add owner to battle during battle creation

--- db/migrations/20211120171553_create_battle_functions.up.sql
+++ db/migrations/20211120171553_create_battle_functions.up.sql
@@ -10,6 +10,7 @@ CREATE OR REPLACE FUNCTION create_battle(
 BEGIN
     INSERT INTO battles (owner_id, name, point_values_allowed, auto_finish_voting, point_average_rounding) VALUES (leaderId, battleName, pointsAllowed, autoVoting, pointAverageRounding) RETURNING id INTO battleId;
     INSERT INTO battles_leaders (battle_id, user_id) VALUES (battleId, leaderId);
+    INSERT INTO battles_users (battle_id, user_id) VALUES (battleId, leaderId);
 END;
 $$ LANGUAGE plpgsql;
 
