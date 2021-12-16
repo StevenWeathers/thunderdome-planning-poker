fe9a035530e149e10e2410f4f4eb992fa1757073
Steven Weathers <steven@weathers.me>
2021-12-16 17:42:37 -0500

    Improve Admin Users table UX

--- frontend/src/pages/admin/Users.svelte
+++ frontend/src/pages/admin/Users.svelte
@@ -9,10 +9,13 @@
     import CheckIcon from '../../components/icons/CheckIcon.svelte'
     import DeleteConfirmation from '../../components/DeleteConfirmation.svelte'
     import SolidButton from '../../components/SolidButton.svelte'
+    import UserAvatar from '../../components/user/UserAvatar.svelte'
     import { warrior } from '../../stores.js'
     import { _ } from '../../i18n.js'
-    import { appRoutes } from '../../config.js'
+    import { AppConfig, appRoutes } from '../../config.js'
     import { validateUserIsAdmin } from '../../validationUtils.js'
+
+    const { AvatarService } = AppConfig
 
     export let xfetch
     export let router
@@ -210,94 +213,174 @@
                 </div>
             </div>
 
-            <table class="table-fixed w-full">
-                <thead>
-                    <tr>
-                        <th class="flex-1 p-2">
-                            {$_('pages.admin.registeredWarriors.name')}
-                        </th>
-                        <th class="flex-1 p-2">
-                            {$_('pages.admin.registeredWarriors.email')}
-                        </th>
-                        <th class="flex-1 p-2">
-                            {$_('pages.admin.registeredWarriors.company')}
-                        </th>
-                        <th class="flex-1 p-2">
-                            {$_('pages.admin.registeredWarriors.rank')}
-                        </th>
-                        <th class="flex-1 p-2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each users as user}
-                        <tr>
-                            <td class="border p-2">
-                                <a
-                                    href="{appRoutes.admin}/users/{user.id}"
-                                    class="no-underline text-blue-500 hover:text-blue-800"
-                                    >{user.name}</a
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div
+                        class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
+                    >
+                        <div
+                            class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+                        >
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            {$_(
+                                                'pages.admin.registeredWarriors.name',
+                                            )}
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            {$_(
+                                                'pages.admin.registeredWarriors.email',
+                                            )}
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            {$_(
+                                                'pages.admin.registeredWarriors.company',
+                                            )}
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            {$_(
+                                                'pages.admin.registeredWarriors.rank',
+                                            )}
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="relative px-6 py-3"
+                                        >
+                                            <span class="sr-only">Actions</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody
+                                    class="bg-white divide-y divide-gray-200"
                                 >
-                                {#if user.country}
-                                    &nbsp;
-                                    <CountryFlag
-                                        country="{user.country}"
-                                        additionalClass="inline-block"
-                                    />
-                                {/if}
-                            </td>
-                            <td class="border p-2">
-                                {user.email}
-                                {#if user.verified}
-                                    &nbsp;
-                                    <span
-                                        class="text-green-600"
-                                        title="{$_(
-                                            'pages.admin.registeredWarriors.verified',
-                                        )}"
-                                    >
-                                        <CheckIcon />
-                                    </span>
-                                {/if}
-                            </td>
-                            <td class="border p-2">
-                                <div>{user.company}</div>
-                                {#if user.jobTitle}
-                                    <div class="text-gray-700 text-sm">
-                                        {$_(
-                                            'pages.admin.registeredWarriors.jobTitle',
-                                        )}:
-                                        {user.jobTitle}
-                                    </div>
-                                {/if}
-                            </td>
-                            <td class="border p-2">{user.rank}</td>
-                            <td class="border p-2">
-                                {#if user.rank !== 'ADMIN'}
-                                    <HollowButton
-                                        onClick="{promoteUser(user.id)}"
-                                        color="blue"
-                                    >
-                                        {$_('promote')}
-                                    </HollowButton>
-                                {:else}
-                                    <HollowButton
-                                        onClick="{demoteUser(user.id)}"
-                                        color="blue"
-                                    >
-                                        {$_('demote')}
-                                    </HollowButton>
-                                {/if}
-                                <HollowButton
-                                    color="red"
-                                    onClick="{toggleDeleteUser(user.id)}"
-                                >
-                                    {$_('delete')}
-                                </HollowButton>
-                            </td>
-                        </tr>
-                    {/each}
-                </tbody>
-            </table>
+                                    {#each users as user, i}
+                                        <tr class:bg-slate-100="{i % 2 !== 0}">
+                                            <td
+                                                class="px-6 py-4 whitespace-nowrap"
+                                            >
+                                                <div class="flex items-center">
+                                                    <div
+                                                        class="flex-shrink-0 h-10 w-10"
+                                                    >
+                                                        <UserAvatar
+                                                            warriorId="{user.id}"
+                                                            avatar="{user.avatar}"
+                                                            avatarService="{AvatarService}"
+                                                            gravatarHash="{user.gravatarHash}"
+                                                            width="48"
+                                                            class="h-10 w-10 rounded-full"
+                                                        />
+                                                    </div>
+                                                    <div class="ml-4">
+                                                        <div
+                                                            class="text-sm font-medium text-gray-900"
+                                                        >
+                                                            <a
+                                                                href="{appRoutes.admin}/users/{user.id}"
+                                                                class="no-underline text-blue-500 hover:text-blue-800"
+                                                                >{user.name}</a
+                                                            >
+                                                            {#if user.country}
+                                                                &nbsp;
+                                                                <CountryFlag
+                                                                    country="{user.country}"
+                                                                    additionalClass="inline-block"
+                                                                    width="32"
+                                                                    height="24"
+                                                                />
+                                                            {/if}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td
+                                                class="px-6 py-4 whitespace-nowrap"
+                                            >
+                                                {user.email}
+                                                {#if user.verified}
+                                                    &nbsp;
+                                                    <span
+                                                        class="text-green-600"
+                                                        title="{$_(
+                                                            'pages.admin.registeredWarriors.verified',
+                                                        )}"
+                                                    >
+                                                        <CheckIcon />
+                                                    </span>
+                                                {/if}
+                                            </td>
+                                            <td
+                                                class="px-6 py-4 whitespace-nowrap"
+                                            >
+                                                <div
+                                                    class="text-sm text-gray-900"
+                                                >
+                                                    {user.company}
+                                                </div>
+                                                <div
+                                                    class="text-sm text-gray-500"
+                                                >
+                                                    {user.jobTitle}
+                                                </div>
+                                            </td>
+                                            <td
+                                                class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                                            >
+                                                {user.rank}
+                                            </td>
+                                            <td
+                                                class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                                            >
+                                                {#if user.rank !== 'ADMIN'}
+                                                    <HollowButton
+                                                        onClick="{promoteUser(
+                                                            user.id,
+                                                        )}"
+                                                        color="blue"
+                                                    >
+                                                        {$_('promote')}
+                                                    </HollowButton>
+                                                {:else}
+                                                    <HollowButton
+                                                        onClick="{demoteUser(
+                                                            user.id,
+                                                        )}"
+                                                        color="blue"
+                                                    >
+                                                        {$_('demote')}
+                                                    </HollowButton>
+                                                {/if}
+                                                <HollowButton
+                                                    color="red"
+                                                    onClick="{toggleDeleteUser(
+                                                        user.id,
+                                                    )}"
+                                                >
+                                                    {$_('delete')}
+                                                </HollowButton>
+                                            </td>
+                                        </tr>
+                                    {/each}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             {#if totalUsers > usersPageLimit}
                 <div class="pt-6 flex justify-center">
