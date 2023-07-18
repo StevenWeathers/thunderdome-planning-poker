406963c481a215263a4f7bca2a7551240fc4cdb5
Steven Weathers <steven@weathers.me>
2023-07-17 21:35:27 -0400

    Add typescript Retro types

---
+++ frontend/src/types/retro.ts
@@ -0,0 +1,62 @@
+export type Retro = {
+    actionItems: Array<RetroAction>
+    brainstormVisibility: string
+    createdDate: string
+    facilitatorCode: string
+    facilitators: Array<string>
+    format: string
+    groups: Array<RetroGroup>
+    id: string
+    items: Array<RetroItem>
+    joinCode: string
+    maxVotes: number
+    name: string
+    ownerId: string
+    phase: string
+    updatedDate: string
+    users: Array<RetroUser>
+    votes: Array<RetroVote>
+}
+
+export type RetroAction = {
+    comments: Array<RetroActionComment>
+    completed: boolean
+    content: string
+    id: string
+    retroId: string
+}
+
+export type RetroActionComment = {
+    comment: string
+    created_date: string
+    id: string
+    retro_id: string
+    updated_date: string
+    user_id: string
+}
+
+export type RetroGroup = {
+    id: string
+    name: string
+}
+
+export type RetroItem = {
+    content: string
+    groupId: string
+    id: string
+    type: string
+    userId: string
+}
+
+export type RetroUser = {
+    active: boolean
+    avatar: string
+    gravatarHash: string
+    id: string
+    name: string
+}
+
+export type RetroVote = {
+    groupId: string
+    userId: string
+}
