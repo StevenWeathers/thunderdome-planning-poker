50b41845baf6af7c2118a917e5c6a6935de8b9a4
Steven Weathers <steven@weathers.me>
2023-07-17 21:35:15 -0400

    Add typescript organization types

---
+++ frontend/src/types/organization.ts
@@ -0,0 +1,22 @@
+export type Organization = {
+    createdDate: string
+    id: string
+    name: string
+    updatedDate: string
+}
+
+export type Department = {
+    createdDate: string
+    id: string
+    name: string
+    updatedDate: string
+}
+
+export type DepartmentUser = {
+    avatar: string
+    email: string
+    gravatarHash: string
+    id: string
+    name: string
+    role: string
+}
