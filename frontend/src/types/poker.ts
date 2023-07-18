1aa1933212f5853ce6e984cdf798f25410e9df7c
Steven Weathers <steven@weathers.me>
2023-07-17 21:11:24 -0400

    Add typescript types for poker game objects

---
+++ frontend/src/types/poker.ts
@@ -0,0 +1,50 @@
+export type PokerGame = {
+    activePlanId?: string
+    autoFinishVoting: boolean
+    createdDate: Date
+    hideVoterIdentity: boolean
+    id: string
+    joinCode?: string
+    leaderCode?: string
+    leaders: Array<String>
+    name: string
+    plans: Array<PokerStory>
+    pointAverageRounding: string
+    pointValuesAllowed: Array<string>
+    updatedDate: Date
+    users: Array<PokerUser>
+    votingLocked: boolean
+}
+
+export type PokerStory = {
+    id: string
+    name: string
+    type: string
+    referenceId?: string
+    link?: string
+    description?: string
+    acceptanceCriteria?: string
+    active: boolean
+    points: string
+    priority: number
+    skipped: boolean
+    voteEndTime: Date
+    voteStartTime: Date
+    votes: Array<PokerStoryVote>
+}
+
+export type PokerStoryVote = {
+    vote: string
+    warriorId: string
+}
+
+export type PokerUser = {
+    abandoned: boolean
+    active: boolean
+    avatar: string
+    gravatarHash: string
+    id: string
+    name: string
+    rank: string
+    spectator: boolean
+}
