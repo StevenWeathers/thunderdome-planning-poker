1e691cfbc6c7387cf81f12b50cebae1a153731d2
Steven Weathers <steven@weathers.me>
2023-10-02 09:17:42 -0400

    Fix typo in pricing page

--- ui/src/pages/subscription/Pricing.svelte
+++ ui/src/pages/subscription/Pricing.svelte
@@ -90,7 +90,7 @@
               d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
               clip-rule="evenodd"></path>
           </svg>
-          <span>Agile Story Mapping (Storybaords)</span>
+          <span>Agile Story Mapping (Storyboards)</span>
         </li>
         <li class="flex items-center space-x-3">
           <svg
