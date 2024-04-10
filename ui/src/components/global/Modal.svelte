e3270f606719cb7257ec2c249147ca0be6476dc5
Steven Weathers <steven@weathers.me>
2024-04-10 13:56:16 -0400

    Update DeleteConfirmation.svelte

--- ui/src/components/global/Modal.svelte
+++ ui/src/components/global/Modal.svelte
@@ -25,7 +25,6 @@
           aria-label="close"
           type="button"
           class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-          data-modal-toggle="deleteModal"
         >
           <CloseIcon class="w-5 h-5" />
           <span class="sr-only">Close modal</span>
